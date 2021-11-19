168fdd476c5448ab8700c916abad461c8b0657d2
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-11-19 14:58:51 -0800

    Add BuySet api call

--- src/brick-api.js
+++
@@ -1,8 +0,0 @@
-//logic here
-//ui in BuySet
-
-function getLegoSetParts(setNum) {
-  "https://rebrickable.com/api/v3/lego/sets/11717-1/parts/?key=&inc_part_details=1&inc_color_details=0";
-}
-
-export { getLegoSetParts };
--- src/components/BuySet.js
+++ src/components/BuySet.js
@@ -1,6 +1,25 @@
 import React from "react";
-import { getLegoSetParts } from "../brick-api.js";
 
 export default function BuySet() {
-  return <div>Buy Set</div>;
+  function handleSubmit(event) {
+    event.preventDefault();
+    const setNum = event.target.setNum.value || 11717;
+    console.log(setNum);
+    fetch(
+      `https://rebrickable.com/api/v3/lego/sets/${setNum}-1/parts/?key=${process.env.REACT_APP_API_KEY}&inc_part_details=1&inc_color_details=0`
+    ).then(
+      (newParts) => console.log(newParts),
+      (error) => console.log(error)
+    );
+  }
+  return (
+    <div>
+      <h2>Buy a New Lego Set</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="setNum">Enter set number: </label>
+        <input name="setNum" type="text" placeholder="11717" />
+        <button>Buy</button>
+      </form>
+    </div>
+  );
 }
